fa69623eb47874f5047c4c6061a5e405d4914ae4
Par Wenaker <par.wenaker@callistaenterprise.se>
2016-11-18 17:24:24 +0100

    Fixat fel import

--- chunked/src/main/java/se/callista/rxjava/chunked/ChunkedServer.java
+++ chunked/src/main/java/se/callista/rxjava/chunked/ChunkedServer.java
@@ -4,16 +4,14 @@ import io.netty.buffer.ByteBuf;
 import io.reactivex.netty.RxNetty;
 import io.reactivex.netty.protocol.http.server.HttpServer;
 import rx.Observable;
-import se.parwen.rxjava.MyIterator;
 
 import java.util.Random;
-import java.util.concurrent.TimeUnit;
 
 public class ChunkedServer {
     public static void main(String[] args) throws InterruptedException {
         HttpServer<ByteBuf, ByteBuf> httpServer = RxNetty.createHttpServer(8080, (request, response) -> {
             Random random = new Random();
-            return Observable.from(MyIterator::new)
+            return Observable.from(RandomNumberIterator::new)
                     .map(i -> String.format("%d\n", random.nextInt()))
                     .doOnNext(System.out::print)
                     .flatMap(response::writeStringAndFlush);
--- chunked/src/main/java/se/callista/rxjava/chunked/RandomNumberIterator.java
+++ chunked/src/main/java/se/callista/rxjava/chunked/RandomNumberIterator.java
@@ -1,4 +1,4 @@
-package se.callista.rxjava;
+package se.callista.rxjava.chunked;
 
 import java.util.Iterator;
 import java.util.Random;
